0b085cec72f231766024eb961358feba0f77f38c
Yixi <lyh1112@gmail.com>
2023-11-05 11:16:18 +0800

    init project

---
+++ ToDoService/Dtos/ToDoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoService.Dtos
+{
+    public class ToDoItem
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsDone { get; set; }
+    }
+}
